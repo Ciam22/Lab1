0584df6beabd6849e0d38207ad75dce817a0bd1e
Ciam22 <drummymajor555@gmail.com>
2020-01-30 14:08:47 -0500

    Initial Commit for MyAge program

---
+++ MyAgeIs.java
@@ -0,0 +1,18 @@
+
+ public class MyAgeIs{
+ 	
+ 	public static void main( String [] args ){
+ 		
+ 		System.out.println("My Age is: ");
+
+		int currentYear = 2020;
+ 		int  yearOfBirth = 2002;
+ 		int myAge = (currentYear - yearOfBirth);
+
+ 		System.out.print(myAge);
+
+ 
+	
+	}
+
+ }
